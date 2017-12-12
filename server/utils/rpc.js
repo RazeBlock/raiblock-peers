e6fe403457de09b7f38a6651188ccbee84059770
soliury <soliury@soliurydeMacBook-Pro.local>
2017-12-13 00:48:28 +0800

    refactor

--- server/utils/rpc.js
+++ server/utils/rpc.js
@@ -1,6 +1,17 @@
+const _ = require('lodash')
 const fetch = require('node-fetch')
 
+const cache = {}
+const cacheTime = 1000 * 2
+
 module.exports = function (body) {
+  let {action} = body || {}
+  if (cache[action] && cache[action].result && cache[action].timestamp && _.isEqual(body, cache[action].params)) {
+    if (new Date().getTime() - cache[action].timestamp < cacheTime) {
+      return Promise.resolve(cache[action].result)
+    }
+  }
+
   return fetch('http://127.0.0.1:7076', {
     body: JSON.stringify(body),
     headers: {
@@ -15,6 +26,11 @@ module.exports = function (body) {
         let err = new Error(data.error)
         err.data = data
         throw err
+      }
+      cache[action] = {
+        params: _.clone(body),
+        result: _.clone(data),
+        timestamp: new Date().getTime()
       }
       return data
     })
