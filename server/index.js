09d641673b97bed6832c275535c3782c77aaed97
soliury <lingyong1992@foxmail.com>
2017-12-30 21:59:27 +0800

    server support

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
+const path = require('path')
 const Koa = require('koa')
 const app = new Koa()
 const router = require('./router')
 const bodyParser = require('koa-bodyparser')
+const historyApiFallback = require('koa2-connect-history-api-fallback');
 
 app.use(bodyParser())
+app.use(historyApiFallback({ whiteList: ['/api'] }))
 app
   .use(router.routes())
   .use(router.allowedMethods())
+
+app.use(require('koa-static')(path.resolve(__dirname, '../dist')));
 
 app.listen(3000, () => {
   console.log('Listen on http://localhost:3000')
